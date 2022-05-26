a510104b11da80b2eea6e8f08adf2bd923ee2fe9
André Ferreira <andredepaulacontato@gmail.com>
2022-05-25 22:36:18 -0300

    Adição de entrada por xml e json

--- src/main/java/br/com/estudo/webservice/controller/UserController.java
+++ src/main/java/br/com/estudo/webservice/controller/UserController.java
@@ -1,21 +1,26 @@
 package br.com.estudo.webservice.controller;
 
 import br.com.estudo.webservice.model.response.UserRest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+
 @RestController
 @RequestMapping("users")
 public class UserController {
 
     @GetMapping(path = "/{userId}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
-    public UserRest getUser(@PathVariable String userId) {
+    public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
         UserRest userRest = new UserRest();
         userRest.setUserId(userId);
         userRest.setFirstName("André");
         userRest.setLastName("Ferreira de Paula");
         userRest.setEmail("andreferreira0@gmail.com");
-        return userRest;
+        return new ResponseEntity<>(userRest, HttpStatus.OK);
     }
+
     @GetMapping
     public String getUsers(@RequestParam(value = "page", defaultValue = "1") Integer page,
                            @RequestParam(value = "limit", defaultValue = "1",required = false) Integer limit,
